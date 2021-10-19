b576f6cf1076d1f68ad20798d5533cc2a1f6f328
matan h <matan.honig2@gmail.com>
2021-10-19 18:22:30 +0300

    move LICENSE

---
+++ example.py
@@ -0,0 +1,9 @@
+import sys
+
+from simple_issue_creator import IssueReporter
+import PySimpleGUI as sg
+
+sg.theme("lightblue7")
+iss = IssueReporter("https://github.com/matan-h/simple-issue-creator", {"python": sys.version, "my-library": "0.3dev"})
+sys.excepthook = iss.error
+1 / 0  # noqa
